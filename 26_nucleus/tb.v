8b22a767822c016219ae5d9fbd39dbb69995b742
Benoît Allard <benoit.allard@gmx.de>
2022-10-31 17:03:53 +0100

    It's almost sad when the tb works fine on the first attempt

---
+++ 26_nucleus/tb.v
@@ -0,0 +1,22 @@
+`timescale 1ps/1ps
+`include "26_nucleus/puzzle.v"
+
+module tb;
+
+    reg start;
+    wire stopped;
+    wire[31:0] tray;
+    wire[4:0] tray_size;
+
+    puzzle uut (start, stopped, tray, tray_size);
+
+    initial begin
+        $dumpvars(0, tb);
+        $monitor("[%03d] start=%0b, tray=%0b, amount=%0d", $time, start, tray, tray_size);
+        start = 0;
+        #50 start = 1;
+        #1 start = 0;
+
+        @(stopped) $finish;
+    end
+endmodule
--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ puzzle19.vvp: 19_entanglement/tb.v 19_entanglement/puzzle.v turingtumble/cells.v
 	vvp $@
 
 puzzle26: puzzle26.vvp
-puzzle26.vvp: 26_nucleus/puzzle.v turingtumble/cells.v turingtumble/board.v
-	verilator --lint-only --timing --top-module puzzle $<
+puzzle26.vvp: 26_nucleus/tb.v 26_nucleus/puzzle.v turingtumble/cells.v turingtumble/board.v
+	verilator --lint-only --timing --top-module tb $<
 	iverilog $< -o $@
 	vvp $@
